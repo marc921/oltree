415964d0dc4aa48a560aa6b83ec14edd7ba36b25
Marc BRUN <marc.brun@mines-paristech.fr>
2019-03-18 22:13:12 +0100

    Adds types, roads, and villages

--- src/hexagon.js
+++ src/hexagon.js
@@ -2,34 +2,77 @@ import React, { Component } from 'react';
 
 class Hexagon extends Component {
 	openMenu() {
-		const { openMenu, i, j} = this.props;
+		const { openMenu, i, j } = this.props;
 		openMenu(i, j);
 	}
 
 	render() {
-		const { radius, i, j, discovered} = this.props;
+		const { radius, i, j, discovered, type, road, connections, village } = this.props;
+		const roadSize = 0.1, villageSize = 0.3;
 
 		const center=[
-			100 + i * 1.52 * radius,
-			100 + (j * 1.76 + (i%2) * 0.88) * radius
+			100 + i * 1.5 * radius,
+			100 + (j * 1.74	 + (i%2) * 0.87) * radius
 		];
 
 		const openMenu = this.openMenu.bind(this);
 
 		let vertices = '';
-
+		let x,y;
 		for (let i = 0; i < 6; i++) {
-			let x = center[0] + Math.cos(i * Math.PI / 3) * radius;
-			let y = center[1] + Math.sin(i * Math.PI / 3) * radius;
+			x = center[0] + Math.cos(i * Math.PI / 3) * radius;
+			y = center[1] + Math.sin(i * Math.PI / 3) * radius;
 			vertices += x+','+y+' ';
-		}	
+		}
+
+		let villageVertices = '';
+		for (let i = 0; i < 6; i++) {
+			x = center[0] + Math.cos(i * Math.PI / 3) * radius * villageSize;
+			y = center[1] + Math.sin(i * Math.PI / 3) * radius * villageSize;
+			villageVertices += x+','+y+' ';
+		}
+
+		let roadVertices = '';
+		if (road && discovered) {
+			for (let i = 0; i < 6; i++) {
+				x = center[0] + Math.cos(i * Math.PI / 3) * radius * roadSize;
+				y = center[1] + Math.sin(i * Math.PI / 3) * radius * roadSize;
+				roadVertices += x+','+y+' ';
+
+				// road connections
+				if (connections.includes(i)) {
+					x = center[0] + Math.cos(i * Math.PI / 3) * radius * roadSize + Math.cos((2*i+1) * Math.PI / 6) * radius * (0.9 - roadSize);
+					y = center[1] + Math.sin(i * Math.PI / 3) * radius * roadSize + Math.sin((2*i+1) * Math.PI / 6) * radius * (0.9 - roadSize);
+					roadVertices += x+','+y+' ';
+					x = center[0] + Math.cos((i+1) * Math.PI / 3) * radius * roadSize + Math.cos((2*i+1) * Math.PI / 6) * radius * (0.9 - roadSize);
+					y = center[1] + Math.sin((i+1) * Math.PI / 3) * radius * roadSize + Math.sin((2*i+1) * Math.PI / 6) * radius * (0.9 - roadSize);
+					roadVertices += x+','+y+' ';
+				}
+			}
+		}
+ 
+		const colors = ['#fc0', '#04f', '#520', '#170', '#0b5', '#b60'];
 
 		return(
-			<polyline
-      	points={vertices}
-      	fill={discovered ? '#0f0' : '#f00'}
-      	onClick={openMenu}
-      />
+			<g>
+				<polyline
+	      	points={vertices}
+	      	fill={discovered ? colors[type] : '#000'}
+	      	onClick={openMenu}
+	      />
+	      {village && discovered &&
+					<polyline
+		      	points={villageVertices}
+		      	fill={'#00a'}
+		      	// 1px black border
+		      />}
+	      {road && discovered &&
+					<polyline
+		      	points={roadVertices}
+		      	fill={'#ff8'}
+		      	// 1px black border
+		      />}
+			</g>
 		);
 	}
 }
--- src/map.js
+++ src/map.js
@@ -4,12 +4,19 @@ import Hexagon from './hexagon';
 class Map extends Component {
 	constructor(props) {
 		super(props);
+		let types = new Array(props.width * props.height);
+		for (var i = types.length - 1; i >= 0; i--) {
+			types[i] = 0;
+		}
 		this.state = {
 			isMenuOpen: false,
 			hi: -1,
 			hj: -1,
-			discovered: new Array(props.width * props.height),
 			notes: new Array(props.width * props.height),
+			discovered: new Array(props.width * props.height),
+			types: types,
+			roads: new Array(props.width * props.height),
+			villages: new Array(props.width * props.height),
 			mapName: ""
 		};
 	}
@@ -23,12 +30,39 @@ class Map extends Component {
 		});
 	}
 
+	changeType(e, i, j) {
+		const { types } = this.state;
+		const { height } = this.props;
+		types[i * height + j] = e.target.value;
+		this.setState({
+			types: types
+		});
+	}
+
 	discover(i, j) {
 		const { discovered } = this.state;
 		const { height } = this.props;
 		discovered[i * height + j] = !discovered[i * height + j];
 		this.setState({
 			discovered: discovered
+		});
+	}
+
+	addRoad(i, j) {
+		const { roads } = this.state;
+		const { height } = this.props;
+		roads[i * height + j] = !roads[i * height + j];
+		this.setState({
+			roads: roads
+		});
+	}
+
+	addVillage(i, j) {
+		const { villages } = this.state;
+		const { height } = this.props;
+		villages[i * height + j] = !villages[i * height + j];
+		this.setState({
+			villages: villages
 		});
 	}
 
@@ -41,7 +75,7 @@ class Map extends Component {
 	}
 
 	saveMap() {
-		const { mapName, discovered, notes } = this.state;
+		const { mapName, discovered, types, roads, villages, notes } = this.state;
 		const { width, height, hexRadius } = this.props;
 		localStorage.setItem(
 			mapName,
@@ -50,6 +84,9 @@ class Map extends Component {
 					height: height,
 					hexRadius: hexRadius,
 					discovered: discovered,
+					types: types,
+					roads: roads,
+					villages: villages,
 					notes:notes
 				}
 			)
@@ -58,22 +95,29 @@ class Map extends Component {
 
 	loadMap() {
 		const { mapName } = this.state;
+		const { handleChange } = this.props;
 		if(localStorage.getItem(mapName) === null){
-			this.props.handleChange('mapWidth', 20);
-			this.props.handleChange('mapHeight', 8);
-			this.props.handleChange('hexRadius', 40);
+			handleChange('mapWidth', 20);
+			handleChange('mapHeight', 8);
+			handleChange('hexRadius', 40);
 			this.setState({
 				discovered: new Array(20 * 8),
-				notes: new Array(20 * 8)
+				types: new Array(20 * 8),
+				roads: new Array(20 * 8),
+				villages: new Array(20 * 8),
+				notes: new Array(20 * 8),
 			});
 		}
 		else{
-			const { width, height, hexRadius, discovered, notes } = JSON.parse(localStorage.getItem(mapName));
-			this.props.handleChange('mapWidth', width);
-			this.props.handleChange('mapHeight', height);
-			this.props.handleChange('hexRadius', hexRadius);
+			let { width, height, hexRadius, discovered, types, roads, villages, notes } = JSON.parse(localStorage.getItem(mapName));
+			handleChange('mapWidth', width);
+			handleChange('mapHeight', height);
+			handleChange('hexRadius', hexRadius);
 			this.setState({
-				discovered: discovered,
+				discovered: discovered ? discovered : new Array(20 * 8),
+				types: types ? types : new Array(20 * 8),
+				roads: roads ? roads : new Array(20 * 8),
+				villages: villages ? villages : new Array(20 * 8),
 				notes: notes
 			});
 		}
@@ -85,32 +129,64 @@ class Map extends Component {
 		});
 	}
 
-	getMapNames(){
-
-	}
-
 	render() {
 		const { width, height, hexRadius } = this.props;
-		const { isMenuOpen, hi, hj, discovered, notes, mapName } = this.state;
+		const { isMenuOpen, hi, hj, notes, discovered, types, roads, villages, mapName } = this.state;
 		const openMenu = this.openMenu.bind(this);
-		const discover = this.discover.bind(this);
 		const addNotes = this.addNotes.bind(this);
 		const saveMap = this.saveMap.bind(this);
 		const loadMap = this.loadMap.bind(this);
 		const mapNameHandler = this.mapNameHandler.bind(this);
-		let map = [];
+		const discover = this.discover.bind(this);
+		const changeType = this.changeType.bind(this);
+		const addRoad = this.addRoad.bind(this);
+		const addVillage = this.addVillage.bind(this);
 
+		// creation of all hexagons
+		let map = [];
 		for (let i = 0; i < width; i++) {
 			let column = [];
 			for (let j = 0; j < height; j++) {
+
+				// roads connections: i is the direction where the road should be drawn in
+				let connections = [];
+				if(roads[(i-1) * height + j]){
+					connections.push(2+(i%2));
+				}
+				if(roads[(i+1) * height + j]){
+					connections.push((5+((i+1)%2))%6);
+				}
+				if(roads[i * height + j-1] && j>0){
+					connections.push(4);
+				}
+				if(roads[i * height + j+1] && j+1<height){
+					connections.push(1);
+				}
+				if((i%2) === 1 && roads[(i-1) * height + j+1] && j+1<height){
+					connections.push(2);
+				}
+				if((i%2) === 1 && roads[(i+1) * height + j+1] && j+1<height){
+					connections.push(0);
+				}
+				if((i%2) === 0 && roads[(i-1) * height + j-1] && j>0){
+					connections.push(3);
+				}
+				if((i%2) === 0 && roads[(i+1) * height + j-1] && j>0){
+					connections.push(5);
+				}
+				
 				column.push(
 					<Hexagon
-					 	key={i* height + j}
+						key={i* height + j}
 						radius={hexRadius}
 						i={i}
 						j={j}
 						openMenu={openMenu}
 						discovered={discovered[i * height + j]}
+						type={types[i * height + j]}
+						road={roads[i * height + j]}
+						connections={connections}
+						village={villages[i * height + j]}
 					/>
 				);
 			}
@@ -145,11 +221,29 @@ class Map extends Component {
 			  			flexDirection: 'column'
 			  		}}
 			  	>
-			  		<button
-			  			onClick={() => discover(hi, hj)}
-			  		>
+			  		<span style={{color: 'white', backgroundColor: 'black', textAlign: 'center'}}>
+			  			i={hi} j={hj}
+			  		</span>
+			  		<button onClick={() => discover(hi, hj)} >
 			  			{discovered[hi * height + hj] ? 'Cover' : 'Discover'}
 			  		</button>
+			  		<select onChange={e => changeType(e, hi, hj)} value={types[hi * height + hj]}>
+			  			<option value={0}>Plaines</option>
+			  			<option value={1}>Mer</option>
+			  			<option value={2}>Montagnes</option>
+			  			<option value={3}>Forêts</option>
+			  			<option value={4}>Marais</option>
+			  			<option value={5}>Collines</option>
+			  		</select>
+			  		{discovered[hi * height + hj] && types[hi * height + hj] != 1 &&
+			  			<div>
+				  			<button onClick={() => addRoad(hi, hj)} >
+					  			{roads[hi * height + hj] ? 'Remove Road' : 'Add Road'}
+					  		</button>
+					  		<button onClick={() => addVillage(hi, hj)} >
+					  			{villages[hi * height + hj] ? 'Remove Village' : 'Add Village'}
+					  		</button>
+					  	</div>}
 			  		<textarea
 			  			onChange={e => addNotes(hi, hj, e)}
 			  			value={notes[hi * height + hj]}
